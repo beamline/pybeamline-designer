c32328624d7d06f77ee81add87abad7713043a41
Andrea Burattin <andrea.burattin@gmail.com>
2024-12-09 09:43:43 +0100

    Fixed connection button disabled

--- src/components/CodeExecutor.vue
+++ src/components/CodeExecutor.vue
@@ -1,75 +1,61 @@
 <template>
-  <v-container class="flex-fill" style="height: 100%;">
+	<v-container class="flex-fill" style="height: 100%;">
 
-    <div v-if="!kernel">
-      <v-alert v-if="error" type="error" class="mb-4" dense>
-        <strong>Error:</strong>
-        {{ error }}
-      </v-alert>
+		<div v-if="!kernel">
+			<v-alert v-if="error" type="error" class="mb-4" dense>
+				<strong>Error:</strong>
+				{{ error }}
+			</v-alert>
 
-      <p>Specify the URL of your Jupyter Kernel Gateway to connect to it:</p>
-      <v-text-field
-        label="Jupyter Kernel Gateway URL"
-        v-model="serverUrl"
-        outlined
-        placeholder="e.g., http://localhost:8888"
-        class="mt-2"
-      ></v-text-field>
-      <v-btn
-        color="primary"
-        class="mt-2 mb-10"
-        @click="connectToServer">
-        Connect
-      </v-btn>
-      <p>Start your Jupyter Kernel Gateway with:</p>
-      <pre class="pa-3" style="overflow: auto; background-color: black; color: #0f0;">$ jupyter-kernelgateway --KernelGatewayApp.allow_origin="*" --KernelGatewayApp.auth_token='' --KernelGatewayApp.allow_headers="Content-Type, Authorization, X-XSRFToken"</pre>
-    </div>
+			<p>Specify the URL of your Jupyter Kernel Gateway to connect to it:</p>
+			<v-text-field label="Jupyter Kernel Gateway URL" v-model="serverUrl" outlined
+				placeholder="e.g., http://localhost:8888" class="mt-2" :disabled="connecting"></v-text-field>
+			<v-btn color="primary" class="mt-2 mb-10" @click="connectToServer">
+				Connect
+			</v-btn>
+			<p>Start your Jupyter Kernel Gateway with:</p>
+			<pre class="pa-3"
+				style="overflow: auto; background-color: black; color: #0f0;">$ jupyter-kernelgateway --KernelGatewayApp.allow_origin="*" --KernelGatewayApp.auth_token='' --KernelGatewayApp.allow_headers="Content-Type, Authorization, X-XSRFToken"</pre>
+		</div>
 
-    <v-row justify="center" v-if="kernel" class="flex-fill" style="height: 100%">
-      <v-col cols="6" class="editor-column">
-        <div class="codemirror-wrapper">
-          <div class="border-thin">
-            <codemirror v-model="pythonCode" ref="editor" style="height: 500px;" />
-          </div>
-          <v-btn
-            color="primary"
-            class="mt-4 mr-2"
-            :disabled="currentFuture"
-            @click="executeCode">
-            <v-icon icon="mdi-play-speed" />
-            Run Code
-          </v-btn>
-          <v-btn 
-            color="error" 
-            class="mt-4" 
-            @click="stopExecution" 
-            :disabled="!currentFuture">
-            <v-icon icon="mdi-stop-circle-outline" />
-            Stop Execution
-          </v-btn>
-        </div>
-      </v-col>
-      <v-col cols="6" class="output-column">
-        <div v-if="output" class="output-wrapper">
-          <pre class="output">{{ output }}</pre>
-        </div>
-        
-      </v-col>
-    </v-row>
-  </v-container>
+		<v-row justify="center" v-if="kernel" class="flex-fill" style="height: 100%">
+			<v-col cols="6" class="editor-column">
+				<div class="codemirror-wrapper">
+					<div class="border-thin">
+						<codemirror v-model="pythonCode" ref="editor" style="height: 500px;" />
+					</div>
+					<v-btn color="primary" class="mt-4 mr-2" :disabled="currentFuture" @click="executeCode">
+						<v-icon icon="mdi-play-speed" />
+						Run Code
+					</v-btn>
+					<v-btn color="error" class="mt-4" @click="stopExecution" :disabled="!currentFuture">
+						<v-icon icon="mdi-stop-circle-outline" />
+						Stop Execution
+					</v-btn>
+				</div>
+			</v-col>
+			<v-col cols="6" class="output-column">
+				<div v-if="output" class="output-wrapper">
+					<pre class="output">{{ output }}</pre>
+				</div>
+
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
 import { KernelManager, ServerConnection } from "@jupyterlab/services";
 
 export default {
-  data() {
-    return {
-      serverUrl: "http://localhost:8888", // Default Jupyter Kernel Gateway URL
-      output: "", // Output from the kernel
-      error: "", // Error messages
-      kernel: null, // Connected kernel instance
-      pythonCode: `from pybeamline.sources import string_test_source
+	data() {
+		return {
+			serverUrl: "http://localhost:8888", // Default Jupyter Kernel Gateway URL
+			output: "", // Output from the kernel
+			error: "", // Error messages
+			connecting: false,
+			kernel: null, // Connected kernel instance
+			pythonCode: `from pybeamline.sources import string_test_source
 from pybeamline.filters import excludes_activity_filter
 
 string_test_source(["ABC", "ACB", "EFG"]).pipe(
@@ -77,111 +63,113 @@ string_test_source(["ABC", "ACB", "EFG"]).pipe(
 ).subscribe(lambda x: print(str(x)))
 
 `, // Default Python code
-      kernelManager: null,
-      currentFuture: null,
-    };
-  },
-  methods: {
-    async connectToServer() {
-      this.error = "";
-      this.output = "";
-      try {
-        const serverSettings = ServerConnection.makeSettings({
-          baseUrl: this.serverUrl,
-          token: "",
-        });
+			kernelManager: null,
+			currentFuture: null,
+		};
+	},
+	methods: {
+		async connectToServer() {
+			this.error = "";
+			this.output = "";
+			this.connecting = true;
+			try {
+				const serverSettings = ServerConnection.makeSettings({
+					baseUrl: this.serverUrl,
+					token: "",
+				});
 
-        this.kernelManager = new KernelManager({ serverSettings });
-        this.kernel = await this.kernelManager.startNew();
-        console.log("Connected to kernel:", this.kernel);
-      } catch (err) {
-        this.error = `Failed to connect to Jupyter Kernel Gateway: ${err.message}`;
-      }
-    },
-    async executeCode() {
-      if (!this.kernel) {
-        this.error = "Not connected to a kernel.";
-        return;
-      }
-      this.output = "";
-      this.error = "";
+				this.kernelManager = new KernelManager({ serverSettings });
+				this.kernel = await this.kernelManager.startNew();
+				console.log("Connected to kernel:", this.kernel);
+				this.connecting = false;
+			} catch (err) {
+				this.error = `Failed to connect to Jupyter Kernel Gateway: ${err.message}`;
+				this.connecting = false;
+			}
+		},
+		async executeCode() {
+			if (!this.kernel) {
+				this.error = "Not connected to a kernel.";
+				return;
+			}
+			this.output = "";
+			this.error = "";
 
-      try {
-        // Initiate code execution
-        const future = this.kernel.requestExecute({ code: this.pythonCode });
-        this.currentFuture = future; // Track the execution
+			try {
+				// Initiate code execution
+				const future = this.kernel.requestExecute({ code: this.pythonCode });
+				this.currentFuture = future; // Track the execution
 
-        // Listen for output
-        future.onIOPub = (msg) => {
-          if (msg.content.text) {
-            this.output += msg.content.text + "\n";
-          }
-        };
+				// Listen for output
+				future.onIOPub = (msg) => {
+					if (msg.content.text) {
+						this.output += msg.content.text + "\n";
+					}
+				};
 
-        // Wait for execution to complete
-        await future.done;
-        this.currentFuture = null; // Clear once done
-      } catch (err) {
-        this.error = `Failed to execute code: ${err.message}`;
-        this.currentFuture = null; // Clear on error
-      }
-    },
-    async stopExecution() {
-      if (this.currentFuture) {
-        this.currentFuture.dispose(); // Stop execution
-        this.currentFuture = null;
-        this.kernel = await this.kernelManager.startNew();
-        this.error = "Execution stopped by the user.";
-      }
-    },
-  },
+				// Wait for execution to complete
+				await future.done;
+				this.currentFuture = null; // Clear once done
+			} catch (err) {
+				this.error = `Failed to execute code: ${err.message}`;
+				this.currentFuture = null; // Clear on error
+			}
+		},
+		async stopExecution() {
+			if (this.currentFuture) {
+				this.currentFuture.dispose(); // Stop execution
+				this.currentFuture = null;
+				this.kernel = await this.kernelManager.startNew();
+				this.error = "Execution stopped by the user.";
+			}
+		},
+	},
 };
 </script>
 
 <style>
 .full-height-row {
-  height: 100%;
-  display: flex;
-  flex-direction: row;
+	height: 100%;
+	display: flex;
+	flex-direction: row;
 }
 
 .editor-column,
 .output-column {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
 }
 
 .editor-column {
-  padding: 10px;
-  overflow: hidden;
+	padding: 10px;
+	overflow: hidden;
 }
 
 .output-column {
-  padding: 10px;
-  overflow: hidden;
+	padding: 10px;
+	overflow: hidden;
 }
 
 .codemirror-wrapper {
-  flex-grow: 1;
-  /* height: 95%; */
-  margin-bottom: 10px;
+	flex-grow: 1;
+	/* height: 95%; */
+	margin-bottom: 10px;
 }
 
 .output-wrapper {
-  height: calc(100% - 20px);
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
+	height: calc(100% - 20px);
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
 }
 
 .output {
-  white-space: pre-wrap;
-  background: #000;
-  color: #0f0;
-  flex-grow: 1;
-  padding: 5px;
-  overflow-x: auto;
+	white-space: pre-wrap;
+	background: #000;
+	color: #0f0;
+	flex-grow: 1;
+	padding: 5px;
+	overflow-x: auto;
 }
-
 </style>
