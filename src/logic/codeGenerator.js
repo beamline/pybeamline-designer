d006eb8f9f5537b689141d1ed263c7156b1970e2
Sotero-Romero <s225145@student.dtu.dk>
2025-02-10 12:47:33 +0100

    Suggestion traverse diagram

--- src/logic/codeGenerator.js
+++ src/logic/codeGenerator.js
@@ -7,31 +7,45 @@ function traverseDiagram(diagram, callback) {
         acc[block.id] = { ...block, visited: false };
         return acc;
     }, {});
-    console.log(blocks)
 
-    const visit = (blockId) => {
-        if (!blockId || blocks[blockId].visited) return;
+    const visit = (block_info) => {
+        const blockId=block_info["id"]
+        const current_string=block_info["log"]
 
         // Mark block as visited
         blocks[blockId].visited = true;
 
 
         // Process the block
-        callback(blocks[blockId]);
-        const t= "jjajj"
-        console.log(blocks[blockId].outputs.reduce((acc,id) => {return [...acc, {"id": id,"log":t}] },[]))
-
+        const new_string=callback(current_string,blocks[blockId]);
 
         // Visit all outputs (following the one-way directional constraint)
+        blocks[blockId].outputs=blocks[blockId].outputs.reduce((acc,id) => {return [...acc, {"id": id,"log":new_string}] },[])
         blocks[blockId].outputs.forEach(visit);
     };
 
     // Find and start traversal from all source blocks (blocks with no `input`)
     Object.values(blocks)
         .filter(block => !block.input)
-        .forEach(block => visit(block.id));
+        .forEach(block => visit({"id": block.id,"log":""}));
+}
+
+let final_string=""
+let counter=0
+function string_generator(current_string, block){
+    let new_string=current_string
+    if (block.category.type=="source"){
+        final_string+="source_"+counter+"="+block.category.name+"\n"
+        new_string+="source_"+counter+".pipe(\n"
+        counter++
+    }else{
+        new_string+=block.category.name+",\n"
+    }
+    if (block.outputs==false){
+        final_string+=new_string+")\n"
+    }
+    return new_string
 }
 // Example: Print block details during traversal
-traverseDiagram(json, (block) => {
-    console.log(`Visiting Block: ${block.id}, Type: ${block.category.type}`);
-});
+traverseDiagram(json, string_generator);
+console.log(final_string)
