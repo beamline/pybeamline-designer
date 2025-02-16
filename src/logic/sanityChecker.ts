52f07da6898913abb0418be59e59e9c6ce7efc7e
Sotero-Romero <s225145@student.dtu.dk>
2025-02-16 15:10:13 +0100

    sanityChecker implemented

---
+++ src/logic/sanityChecker.ts
@@ -0,0 +1,20 @@
+import { Schema , ValidationFailedError} from "@imhonglu/json-schema";
+import { readFileSync } from "fs";
+
+function sanityChecker(diagram:Object): boolean{
+
+    // Read the schema from a separate file
+    const schemaData= JSON.parse(readFileSync("./test.json", "utf8"));
+
+    // Create a schema instance
+    const schema = new Schema(schemaData);
+
+    // Validate the JSON data against the schema
+    if  (schema.validate(diagram)){
+        return true
+    } else {
+        throw new ValidationFailedError("User diagram is not valid")
+    }
+}
+
+export {sanityChecker}
