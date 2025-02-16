89aeb6ac1e0e362a8e0dabec39c5a5c3763d4125
Sotero-Romero <s225145@student.dtu.dk>
2025-02-16 17:20:26 +0100

    refactoring sanityChecker to use ajv library

--- src/logic/sanityChecker.ts
+++ src/logic/sanityChecker.ts
@@ -1,20 +1,39 @@
-import { Schema , ValidationFailedError} from "@imhonglu/json-schema";
-import { readFileSync } from "fs";
+import Ajv from "ajv";
+import { readFileSync,readdirSync } from "fs";
+
+export {sanityChecker}
 
 function sanityChecker(diagram:Object): boolean{
 
+    // Initialize Ajv validator
+    const ajv = new Ajv();
+    const schemasReferences=readdirSync("./schemas")
+    schemasReferences.forEach((reference) => {
+
+        const filePath = "./schemas/"+reference;
+        const schema = JSON.parse(readFileSync(filePath, "utf8"));
+
+        // Add schema to Ajv, using the filename (or a URL-like identifier) as the schema ID
+        ajv.addSchema(schema,reference);
+    });
+
     // Read the schema from a separate file
-    const schemaData= JSON.parse(readFileSync("./test.json", "utf8"));
+    const schemaData= JSON.parse(readFileSync("./schemas/main.json", "utf8"));
+    console.log(schemaData)
 
     // Create a schema instance
-    const schema = new Schema(schemaData);
+    const validator = ajv.compile(schemaData);
 
     // Validate the JSON data against the schema
-    if  (schema.validate(diagram)){
+    if  (validator(diagram)){
         return true
     } else {
-        throw new ValidationFailedError("User diagram is not valid")
+        throw (validator.errors)
     }
 }
 
-export {sanityChecker}
+
+
+const schemaData= JSON.parse(readFileSync("./test.json", "utf8"));
+console.log(schemaData)
+console.log(sanityChecker(schemaData))
