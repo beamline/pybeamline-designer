efbc5f2b8a3ae15ef655fc71b136144d1ba6f005
arturo <arturocortes2004@gmail.com>
2025-02-16 19:14:46 +0100

    Wrote test syntax1

--- src/logic/tests/syntax/syntax.test.ts
+++ src/logic/tests/syntax/syntax.test.ts
@@ -4,3 +4,14 @@ import { expect, test } from '@jest/globals';
 
 //Initial setup
 let pathToTests : string = "src/logic/tests/syntax/";
+
+
+
+//  ########
+//  Test 1 : [source] -> [sink]
+//  ######## string_test_source is not written correctly
+
+
+//  ########
+//  Test 2 : [source] -> [sink]
+//  ######## string_test_source is missing argument 'iterable' (required)
