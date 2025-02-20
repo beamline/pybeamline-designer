8638f2383a72e2c6e1ab671ab3b712bd34e4702d
Sotero-Romero <s225145@student.dtu.dk>
2025-02-20 16:14:49 +0100

    reorganizing schemas and refactoring validator

--- src/logic/sanityChecker.ts
+++ src/logic/sanityChecker.ts
@@ -1,24 +1,43 @@
 import Ajv from "ajv";
 import { readFileSync,readdirSync } from "fs";
-
+import {resolve, dirname} from "path";
+import { fileURLToPath } from "url";
 export {sanityChecker}
+
+function addReference(filePath:string,reference:string, ajv: Ajv){
+    const schema = JSON.parse(readFileSync(filePath, "utf8"));
+    // Add schema to Ajv, using the filename (or a URL-like identifier) as the schema ID
+    ajv.addSchema(schema,reference);
+}
+
+function addAllReferences(directoryPath: string, ajv: Ajv){
+    const schemasReferences=readdirSync(directoryPath)
+    schemasReferences.forEach((reference) => {
+        const filePath = resolve(directoryPath, reference)
+        if (reference.endsWith(".json")) {
+            addReference(filePath, reference, ajv)
+        } else {
+            addAllReferences(filePath, ajv)
+        }
+    })
+}
+
 
 function sanityChecker(diagram:Object): boolean{
 
+    // Create __filename and __dirname
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const path= resolve(__dirname, "./schemas")
+
     // Initialize Ajv validator
     const ajv = new Ajv();
-    const schemasReferences=readdirSync("src/logic/schemas")
-    schemasReferences.forEach((reference) => {
 
-        const filePath = "src/logic/schemas/"+reference;
-        const schema = JSON.parse(readFileSync(filePath, "utf8"));
-
-        // Add schema to Ajv, using the filename (or a URL-like identifier) as the schema ID
-        ajv.addSchema(schema,reference);
-    });
+    //adding reference schemas
+    addAllReferences(path,ajv)
 
     // Read the schema from a separate file
-    const schemaData= JSON.parse(readFileSync("src/logic/schemas/main.json", "utf8"));
+    const schemaData= JSON.parse(readFileSync(resolve(path,"main.json"), "utf8"));
 
     // Create a schema instance
     const validator = ajv.compile(schemaData);
@@ -33,5 +52,5 @@ function sanityChecker(diagram:Object): boolean{
 
 
 
-//const schemaData= JSON.parse(readFileSync("./test.json", "utf8"));
+//const schemaData= JSON.parse(readFileSync("./tests/unit/unit1.json", "utf8"));
 //console.log(sanityChecker(schemaData))
