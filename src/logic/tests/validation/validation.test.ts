6e8980b87f1699e71ba2a5525515b89c19afce9f
arturo <arturocortes2004@gmail.com>
2025-02-24 13:41:30 +0100

    Added and refactored validation tests

--- src/logic/tests/validation/validation.test.ts
+++ src/logic/tests/validation/validation.test.ts
@@ -6,11 +6,12 @@ import { expect, test } from 'vitest';
 //Initial setup
 let pathToTests : string = "src/logic/tests/validation/";
 
-
+/*
 
 //  ########
 //  Test 1 : [source] -> [sinks]
 //  ######## string_test_source is not written correctly
+
 
 
 //  ########
@@ -19,23 +20,19 @@ let pathToTests : string = "src/logic/tests/validation/";
 
 
 
-/*
+
 //  ########
 //  Test 3 : [sinks]  ERROR
 //  ########
 
-test("Test 3: Sink", () => {
-    expect(generateCode(pathToTests + "valid3.json"))
-        .toBe("User diagram is not valid")
-})
 
 
 //  ########
-//  Test 4 : [source]  ERROR
-//  ########
+//  Test 4 : Source -> Miner -> Filter -> Sink
+//  ########  ERROR: Output type of miner not compatible with input of filter
 
-test("Test 4: Source", () => {
-    expect(generateCode(pathToTests + "valid4.json"))
-        .toBe("User diagram is not valid")
-})
+
+
+
+
 */
