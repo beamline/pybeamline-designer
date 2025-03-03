bf85594b98675b5bf948f0981e3e34d57ee8f307
arturo <arturocortes2004@gmail.com>
2025-03-03 15:13:49 +0100

    Moved ajv declaration to codegeneration script instead of sanity check

--- src/logic/codeGenerator.ts
+++ src/logic/codeGenerator.ts
@@ -2,8 +2,13 @@ import {Traverser} from "./deprecated/Traverser.ts"
 import {sanityChecker} from "./sanityChecker.ts";
 import {readFileSync} from "fs";
 import {ExtendedPipeline} from "./Syntax.js";
-import Ajv, {ErrorObject} from "ajv";
 import {Compiler} from "./Compiler.js";
+import {fileURLToPath} from "url";
+import {dirname, resolve} from "path";
+import Ajv from "ajv";
+import {addAllReferences, addKeywords} from "./ajvConfig.js";
+
+
 
 
 function generateCode (filePathToJSON : string) {
@@ -11,9 +16,30 @@ function generateCode (filePathToJSON : string) {
     let userPipeline : ExtendedPipeline = JSON.parse(readFileSync(filePathToJSON, "utf-8"));
     const compiler : Compiler = new Compiler();
 
+
+    // Create __filename and __dirname
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const path= resolve(__dirname, "./schemas")
+
+    // Initialize Ajv validator
+    const ajv = new Ajv({ allErrors: true });
+
+    // Add customizable keywords
+    addKeywords(ajv)
+    //adding reference schemas
+    addAllReferences(path,ajv)
+
+    const schemaData= JSON.parse(readFileSync(resolve(path,"main.json"), "utf8"));
+
+    //Translation layer goes here
+    ////////////////
+
+    ///////////////
+
     try {
-        sanityChecker(userPipeline);
-    } catch (error) {
+        sanityChecker(userPipeline, ajv, schemaData);
+    } catch (error : any) {
         return error.message
     }
 
--- src/logic/sanityChecker.ts
+++ src/logic/sanityChecker.ts
@@ -3,71 +3,14 @@ import { readFileSync,readdirSync } from "fs";
 import {resolve, dirname} from "path";
 import { fileURLToPath } from "url";
 import {ExtendedBlock} from "./Syntax.js";
+import {addKeywords, addAllReferences} from "./ajvConfig.js";
 export {sanityChecker}
 
-function addReference(filePath:string,reference:string, ajv: Ajv){
-    const schema = JSON.parse(readFileSync(filePath, "utf8"));
-    // Add schema to Ajv, using the filename (or a URL-like identifier) as the schema ID
-    ajv.addSchema(schema,reference);
-}
 
-function addAllReferences(directoryPath: string, ajv: Ajv){
-    const schemasReferences=readdirSync(directoryPath)
-    schemasReferences.forEach((reference) => {
-        const filePath = resolve(directoryPath, reference)
-        if (reference.endsWith(".json")) {
-            addReference(filePath, reference, ajv)
-        } else {
-            addAllReferences(filePath, ajv)
-        }
-    })
-}
-
-function addKeywords (ajv : Ajv){
-    ajv.addKeyword({
-        keyword: "validConnections",
-        type: "array",
-        validate: function (_, outputs: string[], __, parentSchema) {
-
-            if (!parentSchema) return false
-            if (outputs.length==0) return true
-
-            // @ts-ignore
-            const blocks: ExtendedBlock[] = parentSchema.rootData.blocks; // Get all blocks
-            const currentBlock = parentSchema.parentData; // The block being validated
-
-            return outputs.every(outputId => {
-
-                // We find the block with the specific ID
-                const targetBlock = blocks.find(block => block.id === outputId);
-
-                // check input and output types match
-                return targetBlock && targetBlock.descriptors.inputType.some(item => currentBlock.descriptors.outputType.includes(item));
-            });
-        },
-        errors: false
-    });
-}
-
-
-function sanityChecker(diagram:Object): boolean{
-
-    // Create __filename and __dirname
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = dirname(__filename);
-    const path= resolve(__dirname, "./schemas")
-
-    // Initialize Ajv validator
-    const ajv = new Ajv({ allErrors: true });
-
-    // Add customizable keywords
-    addKeywords(ajv)
-
-    //adding reference schemas
-    addAllReferences(path,ajv)
+function sanityChecker(diagram:Object, ajv : Ajv, schemaData : any): boolean{
 
     // Read the schema from a separate file
-    const schemaData= JSON.parse(readFileSync(resolve(path,"main.json"), "utf8"));
+
 
     // Create a schema instance
     const validator = ajv.compile(schemaData);
@@ -82,11 +25,16 @@ function sanityChecker(diagram:Object): boolean{
         //@ts-ignore
         const sampledError = validator.errors[0].instancePath;
         //Gets the block id number that raised the problem
+        //@ts-ignore
+
         const match : string = sampledError.match(/\/blocks\/(\d+)/)[1];
 
         if (match !== null) {
+            //@ts-ignore
             const stopBlock = diagram.blocks.filter(element => element.id == match)[0];
+            //@ts-ignore
             const errorBlocks = diagram.blocks.filter( element => stopBlock.outputs.includes(element.id))
+            //@ts-ignore
             const errorBlocksName = errorBlocks.map (element => element.descriptors.name)
             throw (Error(`Error: at block(s) ${errorBlocksName}`))
 
