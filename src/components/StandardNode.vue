ed194c0377e9a42137525e602f1f86bca120fbc1
arturo <arturocortes2004@gmail.com>
2025-03-08 15:02:38 +0100

    Standard node example

---
+++ src/components/StandardNode.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {Handle, Position} from "@vue-flow/core";
+
+
+const props = defineProps(["name", "inType", "outType", "params"])
+
+
+</script>
+
+<template>
+  <div class="standardNode">
+    <p>{{ props.name }}</p>
+    <Handle type="source" :position="Position.Right" />
+    <Handle type="target" :position="Position.Left" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
