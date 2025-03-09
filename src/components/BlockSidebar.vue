6c5331e0a47188306396bb2297974b20a335a142
Sotero-Romero <s225145@student.dtu.dk>
2025-03-09 15:01:30 +0100

    adding basic structure

---
+++ src/components/BlockSidebar.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps(['modelValue']);
+const emit = defineEmits(['update:modelValue']);
+
+
+</script>
+
+<template>
+  <div class="main">
+    <button class="absolute top-2 right-2 text-xl" @click="emit('update:modelValue', null)">
+      ✖
+    </button>
+
+    <h2 class="text-xl font-bold mb-2">Block: {{modelValue.data.name}}</h2>
+    <i class="i">Input type: {{modelValue.data.name}}</i>
+    <i class="i">Output type: {{modelValue.data.name}}</i>
+    <h2 class="text-xl font-bold mb-2">Parameters:</h2>
+
+    <div v-for="(value, key) in modelValue.data.properties" :key="key" class="mb-2">
+      <label v-if="modelValue.data.required && modelValue.data.required.includes(key)">*</label>
+      <label class="block font-bold">{{ key }}:</label>
+      <input v-model="modelValue.data.properties[key]" class="border p-1 rounded w-full" />
+    </div>
+    <h2 class="text-xl font-bold mb-2">Additional information:</h2>
+    <i>tu puta madre en bragas capullo</i>
+  </div>
+
+
+
+
+</template>
+
+<style scoped>
+.i{
+  padding: 5px;
+}
+.main{
+}
+
+</style>
--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -25,25 +25,7 @@
         v-if="selectedNode"
         class="optionTab"
     >
-      <button class="absolute top-2 right-2 text-xl" @click="selectedNode = null">
-        ✖
-      </button>
-      <h2 class="text-xl font-bold mb-2">Node Details</h2>
-
-      <!-- Editable Field -->
-      <label class="block font-semibold">Label:</label>
-      <input
-          v-model="selectedNode.data.parameter1"
-          class="border rounded w-full p-2 mt-1"
-      />
-
-      <label class="block font-semibold">Parameter2:</label>
-
-      <input
-          v-model="selectedNode.data.paramater2"
-          class="border rounded w-full p-2 mt-1"
-      />
-      <p><strong>Type:</strong> {{ selectedNode.type }}</p>
+      <BlockSidebar v-model="selectedNode" />
     </div>
 
   </div>
@@ -56,12 +38,18 @@ import Background from "@/components/Background.vue";
 import StandardNode from "./StandardNode.vue";
 import StartNode from "./StartNode.vue";
 import EndNode from "./EndNode.vue";
+import BlockSidebar from "@/components/BlockSidebar.vue";
 
 // Example nodes and edges
 const nodes = ref ([
-  { id: "1", type: "start", position: { x: 400, y: 200}, data: { name: "example_source", parameters: {"par1":"papaya"} , label: "node 1"} },
-  { id: "2", type: "standard", position: { x: 800, y: 300}, data: { name: "example_intermediate_block"} },
-  { id: "3", type: "end", position: { x: 1200, y: 200 }, data: { name: "example_sink" ,parameters: {"par2":"hehe"} , label: "node 3"} }
+  { id: "1", type: "start", position: { x: 400, y: 200}, data: { name: "example_source", "properties":{
+        "iterable": "required",
+        "scheduler": ""
+      },required:["iterable", "scheduler"] , label: "node 1"} },
+  { id: "2", type: "standard", position: { x: 800, y: 300}, data: { name: "example_intermediate_block", properties:{}} },
+  { id: "3", type: "end", position: { x: 1200, y: 200 }, data: { name: "example_sink" ,"properties":{
+        "on_next": ""
+      } , label: "node 3"} }
 ]);
 
 const edges = ref([
