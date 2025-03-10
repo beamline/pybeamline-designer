70bcb22881b8af4fcaa0ab3be7ba84bd527aa5ae
arturo <arturocortes2004@gmail.com>
2025-03-10 17:03:23 +0100

    Merged again

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -7,10 +7,15 @@
           v-model:edges="edges"
           class="canva"
           @node-click="onNodeClick"
+          :connection-mode="ConnectionMode.Strict"
+          @connect="onConnect"
           @dragover="onDragOver"
           @dragleave="onDragLeave"
           @drop="onDrop"
       >
+        <template #edge-custom="customEdgeProps">
+          <CustomEdge v-bind="customEdgeProps" />
+        </template>
         <template #node-standard="props">
           <StandardNode v-bind="props" />
         </template>
@@ -34,13 +39,33 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref } from "vue";
 import { VueFlow } from "@vue-flow/core";
 import Background from "@/components/Background.vue";
 import StandardNode from "./StandardNode.vue";
 import StartNode from "./StartNode.vue";
 import EndNode from "./EndNode.vue";
+import CustomEdge from "@/components/CustomEdge.vue";
+import {CustomData} from "@/components/edges.js";
+import {Connection, ConnectionMode} from "@vue-flow/core";
+
+function onConnect(params : Connection) {
+  // You can generate a unique id for the new edge.
+  const newEdge = {
+    id: `e${params.source}-${params.target}`,
+    source: params.source,
+    target: params.target,
+    type: 'custom',
+    animated:true
+  };
+  edges.value.push(newEdge);
+}
+
+type CustomEdgeTypes = 'custom'
+
+type CustomEdge = Edge<CustomData, any, CustomEdgeTypes>
+
 import BlockSidebar from "@/components/BlockSidebar.vue";
 import useDragAndDrop from './useDnD.ts'
 
@@ -48,6 +73,9 @@ const { onDragOver, onDrop, onDragLeave, isDragOver } = useDragAndDrop()
 
 // Example nodes and edges
 const nodes = ref ([
+  { id: "1", type: "start", position: { x: 400, y: 200}, data: { name: "example_source", parameters: {"par1":"papaya"} , label: "node 1"} },
+  { id: "2", type: "standard", position: { x: 800, y: 300}, data: { name: "example_intermediate_block"} },
+  { id: "3", type: "end", position: { x: 1200, y: 200 }, data: {  name: "example_sink" ,parameters: {"par2":"hehe"} , label: "node 3"} }
   { id: "1", type: "start", position: { x: 400, y: 200}, data: { name: "example_source", parameters:{
         "iterable": "required",
         "scheduler": ""
@@ -58,9 +86,8 @@ const nodes = ref ([
       } , label: "node 3"} }
 ]);
 
-const edges = ref([
-  { id: "e1-2", source: "1", target: "2" , animated:true},
-  { id: "e2-3", source: "2", target: "3" , animated:true}
+const edges = ref<CustomEdge[]>([
+  { id: "e1-2", source: "1", target: "2" , type: "custom", animated:true},
 ]);
 
 const selectedNode = ref(null);
