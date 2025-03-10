9ab9a4ac92e6bc97e96034e087833db027e5f1ae
Sotero-Romero <s225145@student.dtu.dk>
2025-03-10 17:29:48 +0100

    adding ajvManager to easily access schemas

---
+++ src/logic/tests/ajvManager.ts
@@ -0,0 +1,90 @@
+import Ajv, {AnySchema} from "ajv";
+import {readdirSync, readFileSync} from "fs";
+import {resolve} from "path";
+import {ExtendedBlock} from "../Syntax.js";
+
+class ajvManager extends Ajv{
+
+    private static instance: ajvManager;
+    private constructor(){
+        super({ allErrors: true })
+        this.addKeywords()
+        //adding reference schemas
+        this.addAllReferences("/src/logic/schemas")
+    }
+    // Public method to get the instance of the Singleton
+    public static getInstance(): ajvManager {
+        if (!ajvManager.instance) {
+            ajvManager.instance = new ajvManager(); // Create instance if it doesn't exist
+        }
+        return ajvManager.instance;
+    }
+    public getSchemabyName(name: string, clean: boolean = false){
+        const schema = this.getSchema(name+".json")?.schema
+        if (!schema) {throw new Error("There is no schema with that name")}
+        if (!clean) {return schema}
+        else {return this.cleanSchema(schema)}
+
+    }
+
+    private addReference(filePath:string, reference : string) {
+        const schema = JSON.parse(readFileSync(filePath, "utf8"));
+        // Add schema to Ajv, using the filename (or a URL-like identifier) as the schema ID
+        this.addSchema(schema,reference);
+    }
+
+    private addAllReferences(directoryPath: string){
+        const schemasReferences=readdirSync(directoryPath)
+        schemasReferences.forEach((reference) => {
+            const filePath = resolve(directoryPath, reference)
+            if (reference.endsWith(".json")) {
+                this.addReference(filePath, reference)
+            } else {
+                this.addAllReferences(filePath)
+            }
+        })
+    }
+
+    private addKeywords (){
+        this.addKeyword({
+            keyword: "validConnections",
+            type: "array",
+            validate: function (_, outputs: string[], __, parentSchema) {
+
+                if (!parentSchema) return false
+                if (outputs.length==0) return true
+
+                // @ts-ignore
+                const blocks: ExtendedBlock[] = parentSchema.rootData.blocks; // Get all blocks
+                const currentBlock = parentSchema.parentData; // The block being validated
+
+                return outputs.every(outputId => {
+
+                    // We find the block with the specific ID
+                    const targetBlock = blocks.find(block => block.id === outputId);
+
+
+                    //Checks if the current outputType or target inputType is any
+                    //if so, automatic true
+                    if (targetBlock !== undefined
+                        && (targetBlock.descriptors.inputType?.includes("any")
+                            || currentBlock.descriptors.outputType.includes("any"))) {
+                        return true
+                    }
+                    // check input and output types match
+                    //@ts-ignore
+                    return targetBlock && targetBlock.descriptors.inputType.some(item => currentBlock.descriptors.outputType.includes(item));
+                });
+            },
+            errors: false
+        });
+    }
+
+    private cleanSchema(schema: AnySchema){
+
+    }
+
+
+}
+
+export default ajvManager
