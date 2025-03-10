733215ff8347cccae8299550d62c47c0f41c45e3
arturo <arturocortes2004@gmail.com>
2025-03-10 16:31:26 +0100

    Made nodeCreator, todo: fix schema

--- src/components/NodeCreator.ts
+++ src/components/NodeCreator.ts
@@ -1,11 +1,47 @@
+import {XYPosition} from "@vue-flow/core";
+import {getEventHandlerParams} from "@vue-flow/core/dist/utils/index.js";
+import {validateAdditionalItems} from "ajv/dist/vocabularies/applicator/additionalItems.js";
 
 
-
-async function createNode(name: string, folder: string, id: string) {
+async function createNode(name: string, folder: string, id: string, position : XYPosition) {
     const path = `/src/logic/schemas/${folder}/${name}.json`;
     const files = import.meta.glob("/src/logic/schemas/**/*.json");
     const module = await files[path]();
     const schema = module.default
+
+
+    const keys = Object.keys(schema.properties.parameters.properties);
+    let params = {} as { [key: string]: string | number };
+    for (let key of keys) {
+        params[key] = "";
+    }
+
+    let nodeType = "standard";
+
+    if (schema.properties.descriptors.properties.inputType === null) {
+        nodeType = "start";
+    }
+    else if (schema.properties.descriptors.properties.outputType === null) {
+        nodeType = "end";
+    }
+    else if (schema.required.includes("input")) {
+        nodeType = "union";
+    }
+
+
+    const node = {
+        id : id,
+        position,
+        type : nodeType,
+        data : {
+            name : name,
+            inputType : schema.properties.descriptors.properties.inputType,
+            outputType : schema.properties.descriptors.properties.outputType,
+            parameters : params
+        }
+    }
+
+    return node
 
 }
 
