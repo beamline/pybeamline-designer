0d9e6460bac433cdfd85796d5e77b9e6cd9eb676
Sotero-Romero <s225145@student.dtu.dk>
2025-03-18 21:16:22 +0100

    background

---
+++ src/componentsPrime/background.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Background } from '@vue-flow/background'
+
+</script>
+
+<template>
+  <Background :size="2" :gap="20" pattern-color="black" />
+</template>
+
+<style scoped>
+
+</style>
