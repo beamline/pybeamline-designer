45d319b46a5438c7eaa701e8f913323fc42ab48b
Sotero-Romero <s225145@student.dtu.dk>
2025-03-18 21:15:47 +0100

    added import map

---
+++ src/components/graph/index.ts
@@ -0,0 +1,8 @@
+import StandardNode from "./StandardNode.vue";
+import StartNode from "./StartNode.vue";
+import EndNode from "./EndNode.vue";
+import CustomEdge from "./CustomEdge.vue";
+import UnionNode from "./UnionNode.vue";
+
+
+export {StandardNode, StartNode, EndNode, CustomEdge, UnionNode};
