a6421cb05410ad5cf3fceec0b1334e4bfe3c2276
Sotero-Romero <s225145@student.dtu.dk>
2025-03-18 22:43:20 +0100

    added editor to code

---
+++ src/componentsPrime/CodeGeneratorEditor.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import {python} from "@codemirror/lang-python";
+import {basicSetup} from "codemirror";
+import {Codemirror} from "vue-codemirror";
+import Button from "primevue/button";
+
+const props = defineProps({
+  codeGenerated: String,
+})
+const code = ref(props.codeGenerated);
+</script>
+
+<template>
+  <div class="main">
+    <codemirror
+        v-model="code"
+        :extensions="[python(), basicSetup]"
+        :style="{ height: '600px', border: '1px solid #ddd' }"
+        :autofocus="true"
+    />
+    <Button label="Open Editor" raised/>
+  </div>
+</template>
+
+<style scoped>
+.main{
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
