e3fcc247b3415d0f30fe4e97a08b004d693607bb
Sotero-Romero <s225145@student.dtu.dk>
2025-03-18 21:15:30 +0100

    prepare for launching the prime app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import PrimeApp from "@/PrimeApp.vue";
+import PrimeVue from 'primevue/config';
+import aura from "@primeuix/themes/aura"
+import 'primeicons/primeicons.css';
+
+
+
+
+//const app = createApp(App).mount('#app')
+const app = createApp(PrimeApp)
+app.use(PrimeVue, {
+    // Default theme configuration
+    theme: {
+        preset: aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: false,
+            cssLayer: false
+        }
+    }
+});
+app.mount('#app')
