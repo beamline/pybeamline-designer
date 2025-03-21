84d93ce35f886a35ab25d30bcdae3bcecb7128c7
arturo <arturocortes2004@gmail.com>
2025-03-21 15:50:04 +0100

    Better dot color in background

--- src/components/background.vue
+++ src/components/background.vue
@@ -4,7 +4,7 @@ import { Background } from '@vue-flow/background'
 </script>
 
 <template>
-  <Background :size="2" :gap="20" pattern-color="black" />
+  <Background :size="2" :gap="20" pattern-color="#D3D3D3" />
 </template>
 
 <style scoped>
