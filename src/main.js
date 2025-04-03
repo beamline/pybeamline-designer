bd4a7e62b29fd920a6771926bf0f43b4fe759d24
Sotero-Romero <s225145@student.dtu.dk>
2025-04-03 15:17:27 +0200

    added transitions

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import Tooltip from 'primevue/tooltip';
 import aura from "@primeuix/themes/aura"
 import 'primeicons/primeicons.css';
 import '@vue-flow/core/dist/style.css'
+import AnimateOnScroll from 'primevue/animateonscroll';
+
+
 
 
 
@@ -25,4 +28,5 @@ app.use(PrimeVue, {
     }
 });
 app.directive('tooltip', Tooltip);
+app.directive('animateonscroll', AnimateOnScroll);
 app.mount('#app')
