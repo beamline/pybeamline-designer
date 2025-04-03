81de9d8fedefc6644f6a9ae3f8027f3314b45f17
Sotero-Romero <s225145@student.dtu.dk>
2025-04-03 15:17:15 +0200

    make button and change of tutorial independent

--- src/components/tutorial/TutorialPanel.vue
+++ src/components/tutorial/TutorialPanel.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
-import Tutorial from "@/components/tutorial/Tutorial.vue";
-import { Panel} from "@vue-flow/core";
+import DesignerTutorial from "@/components/tutorial/DesignerTutorial.vue";
 import Button from "primevue/button";
 import Dialog from "primevue/dialog";
+import SelectButton from 'primevue/selectbutton';
+
 import {ref} from "vue";
+import PyBeamlineTutorial from "@/components/tutorial/PyBeamlineTutorial.vue";
 const tutorial = ref(false);
+const value = ref("Designer")
 </script>
 
 <template>
   <Button label="?" rounded @click="tutorial = true"/>
 
-    <Dialog v-model:visible="tutorial" modal :show-header="false" style="padding:0">
+    <Dialog v-model:visible="tutorial" modal :show-header="false" style="padding:0; width: 1300px; height: 750px; overflow: hidden; border-radius: 11px">
       <template #container="{closeCallback}">
-        <Tutorial :func="closeCallback"/>
+        <SelectButton v-model="value" :options="['Pybeamline','Designer']" style="position: absolute;left: 50%;transform: translateX(-50%); z-index: 7"></SelectButton>
+        <Button icon="pi pi-times" @click="closeCallback" rounded variant="text" style="left:96%; position:absolute" severity="secondary" />
+        <div v-if="value === 'Designer'">
+          <DesignerTutorial/>
+        </div>
+        <div v-else-if="value === 'Pybeamline'">
+          <PyBeamlineTutorial/>
+        </div>
       </template>
     </Dialog>
 </template>
