48cb853e0982dd88d9c6e0a39292e3c7913a5b32
Sotero-Romero <s225145@student.dtu.dk>
2025-04-04 16:09:04 +0200

    added datatable with types

--- src/components/tutorial/PyBeamlineTutorial.vue
+++ src/components/tutorial/PyBeamlineTutorial.vue
@@ -3,18 +3,33 @@ import Card from 'primevue/card';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import InputText from 'primevue/inputtext';
-import {ref, watch} from "vue";
-
-const products=ref([
-  {
-    code: "papaya",
-    name: "ll",
-    category: "stupid",
-    quantity: "mamamia"
-  }
-])
+import {computed, ref} from "vue";
+import ajvManager from "@/logic/AjvManager.js";
 
 
+
+const ajv = ajvManager.getInstance()
+const keys=Object.keys(ajv.schemas).slice(1).filter((key) => (key != "main.json" && key != "definitions.json"))
+const products = keys.map(key => (ajv.getCleanSchemaByName(key.slice(0,-5)))).map((schema) => ({
+  name: schema.descriptors.name,
+  inputType: schema.descriptors.inputType,
+  multipleInputs: !!schema.input,
+  outputType: schema.descriptors.outputType,
+  multipleOutputs: true,
+  parameters:  Object.entries(schema.parameters).map(([key, value]) => (
+      {
+    attrName: key,
+    required:schema.required? schema.required.includes(key) : false,
+  }))
+}))
+
+
+const searchQuery = ref("")
+const filteredData = computed(()=>{
+  return products.filter((product)=> product.name.includes(searchQuery.value))
+})
+
+const expandedRows = ref({});
 
 </script>
 
@@ -67,7 +82,7 @@ const products=ref([
             Filters are operators that do not modify the input stream but selectively allow or block events based on a predicate test. They process an event stream and output a filtered event stream.
           </p>
           <div class="example">
-            result = mysource.pipe( <br> &emsp; &emsp; &emsp; myoperator1(...),<br>&emsp; &emsp; &emsp;
+            result = source_1.pipe( <br> &emsp; &emsp; &emsp; myoperator1(...),<br>&emsp; &emsp; &emsp;
                      myoperator2(...)
             )
           </div>
@@ -85,10 +100,26 @@ const products=ref([
         </template>
       </Card>
     </div>
-    <h2 style="padding-left:50px; width:100%;">Functions List</h2>
+    <h2 style="padding-left:50px; width:100%; padding-bottom: 20px">Functions List</h2>
 
+    <InputText v-model="searchQuery" placeholder="Search..." style="width: 50rem" />
+    <div style="min-height: 50rem">
+      <DataTable v-model:expandedRows="expandedRows" :value="filteredData" tableStyle="min-width: 50rem; padding-bottom: 30%" dataKey="name">
+        <Column expander style="width: 5rem" />
+        <Column field="name" header="Name"></Column>
+        <Column field="inputType" header="Input type"></Column>
+        <Column field="multipleInputs" header="Allow multiple inputs"></Column>
+        <Column field="outputType" header="Output type"></Column>
+        <Column field="multipleOutputs" header="Allow multiple outputs"></Column>
 
-
+        <template #expansion="slotProps">
+          <DataTable :value="slotProps.data.parameters">
+            <Column field="attrName" header="Attribute Name"></Column>
+            <Column field="required" header="Required"></Column>
+          </DataTable>
+        </template>
+      </DataTable>
+    </div>
 
   </div>
 </template>
